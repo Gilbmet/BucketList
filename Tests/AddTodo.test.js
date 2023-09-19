d711744053b7832dc8357b51bc7c9d1d13ad209a
Gilbmet <gilbmet254@gmail.com>
2023-09-19 13:47:37 +0300

    Create AddTodo.test.js

---
+++ Tests/AddTodo.test.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import AddTodo from './AddTodo'; // Import the component you want to test
+
+describe('AddTodo component', () => {
+  it('renders correctly', () => {
+    // Write your test case here
+    // For example, render the component and make assertions about its rendering
+  });
+
+  // Add more test cases as needed
+});
