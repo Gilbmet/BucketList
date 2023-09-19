7265de3c661a688092793269145ce6c7919354ef
Gilbmet <gilbmet254@gmail.com>
2023-09-19 13:48:35 +0300

    Create AddTodoButton.test.js

---
+++ Tests/AddTodoButton.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import AddTodoButton from './AddTodoButton'; // Import the component you want to test
+
+describe('AddTodoButton component', () => {
+  it('renders correctly', () => {
+    // Write your test case here
+  });
+
+  // Add more test cases as needed
+});
