7c968540aab8cf4056c7d8e676062571c5be665c
Gilbmet <gilbmet254@gmail.com>
2023-09-19 13:49:02 +0300

    Create Header.test.js

---
+++ Tests/Header.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import Header from './Header'; // Import the component you want to test
+
+describe('Header component', () => {
+  it('renders correctly', () => {
+    // Write your test case here
+  });
+
+  // Add more test cases as needed
+});
