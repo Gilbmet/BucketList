8425565c76198c754b39250e1ac48f19d21164e7
Gilbmet <gilbmet254@gmail.com>
2023-09-19 13:50:35 +0300

    Create TodosContainer.test.js

---
+++ Tests/TodosContainer.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import TodosContainer from './TodosContainer'; // Import the component you want to test
+
+describe('TodosContainer component', () => {
+  it('renders correctly', () => {
+    // Write your test case here
+  });
+
+  // Add more test cases as needed
+});
