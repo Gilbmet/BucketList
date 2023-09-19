75119f20c26b59e96e8f47dd26c15a43189d4e36
Gilbmet <gilbmet254@gmail.com>
2023-09-19 13:49:36 +0300

    Create TodoItem.test.js

---
+++ Tests/TodoItem.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { render } from '@testing-library/react-native';
+import TodoItem from './TodoItem'; // Import the component you want to test
+
+describe('TodoItem component', () => {
+  it('renders correctly', () => {
+    // Write your test case here
+  });
+
+  // Add more test cases as needed
+});
